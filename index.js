96cdfb2b59e7369ab9c9fe7d8d58cef665b27542
Anton Edigarev <konanart@mail.ru>
2024-06-16 13:46:57 +0300

    version 1.0

---
+++ index.js
@@ -0,0 +1,181 @@
+
+const { hydrate } = require('@grammyjs/hydrate');
+
+const path = require('path');
+const process = require('process');
+const fs = require('fs');
+
+// install required classes
+const { Bot, GrammyError, HttpError, InlineKeyboard, } = require('grammy');
+
+const database = require('./sheets-database.js');
+
+const BOT_TOKEN_PATH = path.join(process.cwd(), 'bot-token.json');
+
+const file_content = fs.readFileSync(BOT_TOKEN_PATH);
+const bot_token = JSON.parse(file_content).bot_token;
+
+process.env.BOT_TOKEN = bot_token;
+const bot = new Bot(process.env.BOT_TOKEN); 
+bot.use(hydrate());
+
+
+//error handler
+bot.catch((err) => { const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`); const e = err.error;   
+    if (e instanceof GrammyError) {
+     console.error('Error in request:', e.description);
+    } else if (e instanceof HttpError) { console.error('Could not contact Telegram:', e);
+    } else {
+    console.error('Unknown error:', e);
+    }
+    });
+
+// menu
+
+bot.api.setMyCommands([
+    { command: 'start', description: 'Запуск бота' },
+    { command: 'topic', description: 'Предложить тему' },
+    { command: 'project', description: 'Рассказать о проекте' },
+    { command: 'improvements', description: 'Оставить пожелания' },
+    ]);
+
+// keybord of starting message
+const startKeyboard = new InlineKeyboard()
+.text('Предложить тему', 'topic_btn').row()
+.text('Рассказать о проекте', 'project_btm').row()
+.text('Оставить пожелания', 'improve_btm');
+
+const backKeyboard = new InlineKeyboard().text('< Назад', 'back_btn');
+
+
+
+const defaultSheetName = 'Несортированные!';
+
+let sheetName = defaultSheetName;
+
+//
+function getButtonMetaData(buttonName)
+{
+    if(buttonName === 'topic_btn')
+    {
+      sheetName = 'Темы!';
+      return 
+    }
+
+    if(buttonName === 'project_btm')
+    {
+      sheetName = 'Проекты!';
+      return 
+    }
+
+    if(buttonName === 'improve_btm')
+    {
+      sheetName = 'Пожелания!';
+      return 
+    }
+
+    if(buttonName === 'back_btn')
+    {
+      sheetName = 'Несортированные!';
+      return 
+    }
+
+}
+
+
+// message when a user starts conversation
+bot.command('start', async (ctx) => 
+{ await ctx.reply('Привет, это бот «Цифровой истории»!\n\nСюда можно отправить любые предложения, новости и пожелания. \n\nО чем хотите написать?', 
+{ reply_markup: startKeyboard,});
+});
+
+//handlers for start keyboard buttons
+bot.callbackQuery('topic_btn', async (ctx) => {
+await ctx.callbackQuery.message.editText('О чем нам рассказать?', {
+reply_markup: backKeyboard,
+});
+await ctx.api.editMessageText 
+await ctx.answerCallbackQuery()
+getButtonMetaData('topic_btn');
+});
+
+bot.callbackQuery('project_btm', async (ctx) => {
+await ctx.callbackQuery.message.editText('Чем Вы занимаетесь?\n\nРасскажите в нескольких предложениях о проекте и оставьте контакт для связи.', { reply_markup: backKeyboard,
+});
+await ctx.api.editMessageText 
+await ctx.answerCallbackQuery()
+getButtonMetaData('project_btm');
+});
+
+bot.callbackQuery('improve_btm', async (ctx) => {
+await ctx.callbackQuery.message.editText('Любые идеи и конструктивная критика приветствуются. Помогите нам стать лучше!', { reply_markup: backKeyboard,
+});
+await ctx.api.editMessageText 
+await ctx.answerCallbackQuery()
+getButtonMetaData('improve_btm');
+});
+
+bot.callbackQuery('back_btn', async (ctx) => {
+await ctx.callbackQuery.message.editText('Привет, это бот «Цифровой истории»!\n\nСюда можно отправить любые предложения, новости и пожелания. \n\nО чем хотите написать?', { reply_markup: startKeyboard,
+});
+await ctx.api.editMessageText 
+await ctx.answerCallbackQuery()
+getButtonMetaData('back_btn');
+});
+
+//command handlers
+bot.command('topic',async (ctx) => {
+  await  ctx.reply('О чем нам рассказать?', {reply_markup: backKeyboard,});
+  getButtonMetaData('topic_btn');
+  });
+
+bot.command('project',async (ctx) => {
+  await  ctx.reply('Чем Вы занимаетесь?\n \nРасскажите в нескольких предложениях о проекте и оставьте контакт для связи.', {reply_markup: backKeyboard,});
+  getButtonMetaData('project_btm');
+  });
+
+bot.command('improvements',async (ctx) => {
+  await  ctx.reply('Любые идеи и конструктивная критика приветствуются. Помогите нам стать лучше!', {reply_markup: backKeyboard,});
+  getButtonMetaData('improve_btm');
+});
+
+
+async function returnMenu (ctx)
+{
+  await ctx.reply('Спасибо, что нашли время и написали нам! \n \n Хотите еще что-нибудь написать?', {reply_markup: startKeyboard,});
+}
+
+
+
+bot.start();
+console.log('Бот запущен');
+
+
+bot.on("message", async (ctx) => {
+  const message = ctx.message;
+  const utcDate = new Date(message.date*1000);
+  const date = utcDate.toLocaleString();
+
+  const mesInfo = [[message.chat.id, message.chat.username , message.chat.first_name,message.chat.last_name,date,message.text]];
+
+  (async () =>
+  {
+    const auth = await database.authorize();
+    try 
+    {
+      const lastMessageNumber = await database.getMessagesNumber(auth,sheetName);
+      const counter = database.counterStartValue + lastMessageNumber;
+      const currentCell = sheetName + database.startColomn + counter;
+      
+      database.writeFeedback(auth,mesInfo,currentCell);
+    
+      returnMenu(ctx);
+
+      sheetName = defaultSheetName;
+
+    } catch (error) {
+      console.log(error);
+    }
+  })()
+});
---
+++ sheets-database.js
@@ -0,0 +1,138 @@
+const fs = require('fs');
+const fs_async = require('fs').promises;
+
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+const {GoogleAuth} = require('google-auth-library');
+
+// If modifying these scopes, delete token.json and then refresh
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']; //this scope for editing sheets. More scopes at https://developers.google.com/identity/protocols/oauth2/scopes#sheets
+
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+
+
+const SHEET_ID_PATH = path.join(process.cwd(), 'sheet-id.json');
+const id_file_content = fs.readFileSync(SHEET_ID_PATH);
+const sheet_ID = JSON.parse(id_file_content).sheet_id;
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs_async.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAuth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+ 
+async function saveCredentials(client) {
+  const content = await fs_async.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs_async.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    access_type: 'offline',
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    console.log(client.credentials);
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+
+
+
+const startColomn = 'A';
+const counterStartValue = 2; //2 row - in the 1 colums name - wtf??
+
+async function getMessagesNumber(auth,sheetPage)
+{
+  const sheets = google.sheets({version: 'v4', auth});
+
+  const counterCell = sheetPage + 'H3';
+
+  const result = await sheets.spreadsheets.values.get({
+    spreadsheetId: sheet_ID,
+    range: counterCell, 
+  });
+
+  const messageCounter = Number(result.data.values);
+  return messageCounter;
+}
+
+
+async function writeFeedback(auth,values, cell) 
+{
+  const sheets = google.sheets({version: 'v4', auth});
+
+  const resource = {
+    values,
+  };
+
+  const range = cell;
+
+  const result = await sheets.spreadsheets.values.update({
+    spreadsheetId: sheet_ID,
+    range, 
+    valueInputOption: 'USER_ENTERED',
+    resource,
+  });
+
+  console.log('%d cells updated.', result.data.updatedCells);
+  return result;
+}
+
+
+module.exports.authorize = authorize;
+module.exports.getMessagesNumber = getMessagesNumber;
+module.exports.writeFeedback = writeFeedback;
+
+exports.startColomn = startColomn;
+exports.counterStartValue = counterStartValue;
